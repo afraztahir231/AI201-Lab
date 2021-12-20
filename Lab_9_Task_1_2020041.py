72911a6323a925ad0e05a03dd54883345d92e87b
King Bradley <afraztahir123@gmail.com>
2021-12-20 17:24:34 +0500

    Added File

---
+++ Lab_9_Task_1_2020041.py
@@ -0,0 +1,39 @@
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(string, key):
+    arr = []
+
+    for x in string:
+        if x != " ":
+            arr.append(alphabets[(alphabets.index(x) + key)%26])
+
+        else:
+            arr.append(" ")
+
+    for y in arr:
+        print(y, end= "")
+
+def decrypt(string, key):
+    arr = []
+
+    for x in string:
+        if x != " ":
+            arr.append(alphabets[(alphabets.index(x) - key)%26])
+        else:
+            arr.append(" ")
+    
+    for y in arr:
+        print(y, end = "")
+
+
+e_mess = input("Enter Anything in small caps(encrypted): ")
+shift_key = int(input("Enter Key: "))
+
+decrypt(e_mess, shift_key)
+print("\n")
+
+d_mess = input("Enter Anything in small caps(decrypted): ")
+shift_key = int(input("Enter Key: "))
+
+encrypt(d_mess, shift_key)
+print("\n")
